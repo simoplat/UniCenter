a4b01896fab2fb6ddb951f79b29f1ad45442a5bd
simoplat <150624084+simoplat@users.noreply.github.com>
2026-03-06 20:08:49 +0100

    Update Utente.java

--- unicenter/src/main/java/it/project/Utente.java
+++ unicenter/src/main/java/it/project/Utente.java
@@ -1,7 +1,7 @@
 package it.project;
 import java.util.Scanner;
 
-public class Utente {
+public abstract class Utente {
     
     private int id;
     private String nome;
@@ -10,7 +10,6 @@ public class Utente {
     private String password;
     private String codiceFiscale;
 
-    
 
     public Utente(int id, String nome, String cognome, String email, String password, String codiceFiscale) {
         this.id = id;
@@ -21,10 +20,56 @@ public class Utente {
         this.codiceFiscale = codiceFiscale;
     }
 
-
-
     public void menuPersonale(Scanner sc) {
         System.out.println("Menu personale");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
     }
     
 }
